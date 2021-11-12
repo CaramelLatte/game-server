88d85248be84ee089369137eb1804ab30b8b534d
CaramelLatte <69405080+CaramelLatte@users.noreply.github.com>
2021-11-12 12:39:00 -0800

    updated router

--- app.py
+++ app.py
@@ -1,27 +1,26 @@
-# Python 3 server example
-from http.server import BaseHTTPRequestHandler, HTTPServer
-import time
-from routes.minecraft import routes
+from flask import Flask
+from routes.minecraft import check, normalize, closeGame
 
-hostName = "0.0.0.0"
-serverPort = 9999
+app = Flask(__name__)
 
-class MyServer(BaseHTTPRequestHandler):
-    def do_GET(self):
-        self.send_response(200)
-        self.send_header("Content-type", "text/html")
-        self.end_headers()
-        route_content = routes[self.path]
-        return bytes(route_content, "UTF-8")
+@app.route("/")
+def startGame():
+  normalize()
+  return "<p>Starting game</p>"
+@app.route("/close")
+def close():
+  returnval = closeGame()
+  return returnval
+@app.route("/check")
+def check_status():
+  returnval = check()
+  return returnval
 
-if __name__ == "__main__":        
-    webServer = HTTPServer((hostName, serverPort), MyServer)
-    print("Server started http://%s:%s" % (hostName, serverPort))
 
-    try:
-        webServer.serve_forever()
-    except KeyboardInterrupt:
-        pass
 
-    webServer.server_close()
-    print("Server stopped.")
+if __name__ == "__main__":
+    from waitress import serve
+    host = "0.0.0.0"
+    port = 8080
+    serve(app, host=host, port=port)
+    print("Running server on ", host, port)
--- routes/minecraft.py
+++ routes/minecraft.py
@@ -1,4 +1,32 @@
+import os
+import pyautogui as ag
+import pygetwindow as gw
 routes = {
   "/" : "Play Minecraft",
   "/goodbye" : "Stop playing minecraft"
 }
+
+def closeGame():
+  try:
+    game_server = gw.getWindowsWithTitle("Minecraft server")[0]
+    game_server.close()
+  except:
+    return "<p>Game not running</p>"
+  else:
+    return "<p>Game server closed!</p>"
+
+def normalize():
+  try:
+    game_server = gw.getWindowsWithTitle("Minecraft server")[0]
+  except:
+    launch = "y"
+    if launch == "y":
+      os.startfile("minecraft.bat", 'open')
+
+def check():
+  try:
+    game_server = gw.getWindowsWithTitle("Minecraft server")[0]
+  except:
+    return "<p>Game is not running!</p>"
+  else:
+    return "<p>Game is online!</p>"
