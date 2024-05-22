17551d475f645dbafbe64a0197ab76781a7ab390
Jared <jared.ekenstam@gmail.com>
2024-05-22 07:40:06 -0700

    Reworked old server code to work in Linux headless environment

---
+++ games.py
@@ -0,0 +1,72 @@
+import os
+import keyboard
+import time
+
+def clear_terminal_inputs():
+    keyboard.press_and_release("enter")
+    time.sleep(1)
+
+def parse_text(text):
+    special_chars = {"~":"`","!":"1","@":"2","#":"3","$":"4","%":"5","^":"6","&":"7","*":"8","(":"9",")":"0","_":"-","+":"=","<":",",">":".","?":"/","{":"[",", }":"]","|":"\\"}
+    for char in text:
+        if char.isalpha() == True:
+            if char.isupper() == True:
+                char = char.lower()
+                hotkey = "shift"
+                keyboard.press_and_release(f"{hotkey}+" + char)
+            else:
+                keyboard.write(char)
+                
+        elif char in special_chars.keys():
+            hotkey = "shift"
+            keyboard.press_and_release(f"{hotkey}+" + special_chars[char])
+        else:
+            keyboard.write(char)
+
+class GameServer:
+    def __init__(self, name, port, path, log_file, cmds) -> None:
+        self.name = name
+        self.port = port
+        self.path = path
+        self.log_file = log_file
+        self.cmds = cmds
+        self.running = False
+
+    def check_if_running(self):
+        self.running = False
+        os.system("ps -ef > running_jobs.txt")
+        f = open("running_jobs.txt", "r")
+        file_contents = f.readlines()
+        for line in file_contents:
+            if self.cmds["launch"] in line:
+                self.running = True
+            else:
+                pass
+
+    def launch(self):
+        self.check_if_running()
+        if not self.running:
+            print(f"{self.name} server not found, launching")
+            clear_terminal_inputs()
+            keyboard.write("cd " + self.path)
+            keyboard.press_and_release("enter")
+            self.exec_cmd("launch")
+        else:
+            print(f"{self.name} already running")
+
+    def exec_cmd(self, command):
+        clear_terminal_inputs()
+        hotkeys = ["ctrl", "shift", "alt"]
+        for key in hotkeys:
+            if key in self.cmds[command]:
+                hotkey = self.cmds[command].split(",")
+                keyboard.press_and_release(f"{hotkey[0]}+{hotkey[1]}")
+                return command
+        keyboard.write(self.cmds[command])
+        keyboard.press_and_release("enter")
+        return command
+game_list = []
+
+minecraft_serv = GameServer("Minecraft", [25565], "/home/gameserver/minecraft/", {"file": "/home/gameserver/minecraft/logs/latest.log",  "connect": "joined the game", "disconnect": "left the game", "splice_start": 33}, {"launch": "java -xmx15g -xms15g -jar server.jar nogui", "stop": "/stop", "test": "shift,s"})
+
+game_list.append(minecraft_serv)
---
+++ server.py
@@ -0,0 +1,120 @@
+from flask import Flask
+from flask_cors import CORS
+from threading import Timer
+from time import sleep
+from games import *
+import datetime
+import json
+
+app = Flask(__name__)
+CORS(app)
+active_server = ""
+player_count = 0
+delay = False
+hosting = False
+delay_time = datetime.datetime.now()
+connected_players = []
+
+def checktime():
+    global delay
+    global delay_time
+    check_time = datetime.datetime.now()
+    difference = (check_time.minute + (check_time.hour * 60)) - (delay_time.minute + (delay_time.hour * 60))
+    if difference >= 10:
+        delay = False
+    else:
+        return
+    
+class RepeatedTimer(object):
+    def __init__(self, interval, function):
+        self._timer = None
+        self.interval = interval
+        self.function = function
+        self.is_running = False
+        self.start()
+    def _run(self):
+        self.is_running = False
+        self.start()
+        self.function()
+
+    def start(self):
+        if not self.is_running:
+            self._timer = Timer(self.interval, self._run)
+            self._timer.start()
+            self.is_running = True
+
+    def stop(self):
+        self._timer.cancel()
+        self.is_running = False
+
+def update_status():
+    checktime()
+
+    global connected_players
+    global active_server
+    global player_count
+    active_server = ""
+    for game in game_list:
+        game.check_if_running()
+        if game.running == True:
+            break
+    if game.running == True:
+        active_server = game.name
+        file = open(game.log_file["file"], 'r')
+
+        for line in file:
+            if game.log_file["connect"] in line:
+                parsed_name = line[game.log_file["splice_start"]:].strip("\n").replace(game.log_file["connect"], "").replace(" ", "")
+                if not parsed_name in connected_players:
+                    connected_players.append(parsed_name)
+            elif game.log_file["disconnect"] in line:
+                parsed_name = line[game.log_file["splice_start"]:].strip("\n").replace(game.log_file["disconnect"], "").replace(" ", "")
+                if(parsed_name in connected_players):
+                    connected_players.remove(parsed_name)
+        file.close()
+
+rt = RepeatedTimer(10, update_status)
+
+@app.route('/')
+def home():
+    return "No. Go away."
+@app.route('/update')
+def serv_stats():
+    return json.dumps({"active_server" : active_server, "player_count" : len(connected_players), "returnval" : "0"})
+@app.route('/minecraft')
+def minestats():
+    return json.dumps({"active_server" : active_server, "player_count" : len(connected_players), "returnval" : "0"})
+@app.route('/minecraft/start')
+def start_mine():
+    global delay
+    global delay_time
+    global active_server
+    if not delay and active_server == "":
+        delay = True
+        delay_time = datetime.datetime.now()
+        active_server = "minecraft"
+        returnval = minecraft_serv.exec_cmd("launch")
+        return json.dumps({"active_server" : active_server, "player_count": len(connected_players), "returnval": returnval})
+    else:
+        return("Server is leased. Please try again later.")
+@app.route("/minecraft/stop")
+def stop_mine():
+    global active_server
+    global delay
+    if active_server == "minecraft":
+        delay = False
+        returnval = minecraft_serv.exec_cmd("stop")
+        active_server = ""
+        return json.dumps({"active_server" : active_server, "player_count": player_count, "returnval": returnval})
+    else:
+        return json.dumps({"active_server" : active_server, "player_count": player_count, "returnval": "Minecraft Server not running"})
+    
+
+if __name__ == "__main__":
+    try:
+      print("Attempting to start HTTPS server.")
+      app.run(ssl_context=('/etc/letsencrypt/archive/games.caramellatte.dev/fullchain1.pem', '/etc/letsencrypt/archive/games.caramellatte.dev/privkey1.pem'), host="0.0.0.0", port=443)
+    except:
+        print("Server cannot start, aborting")
+
+rt.stop()
