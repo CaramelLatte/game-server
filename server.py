9b8e2e1b3bb7340c8f2ddedd85ecc10b1e31b89d
Jared <jared.ekenstam@gmail.com>
2024-05-29 12:47:15 -0700

    Attempting palworld

--- server.py
+++ server.py
@@ -57,9 +57,9 @@ def update_status():
     active_server = ""
     for game in game_list:
         game.check_if_running()
-        if game.running == True and game.log["file"] is not None:
+        if game.running == True and game.log["file"] != None:
             active_server = game.name
-            
+
             file = open(game.log_file["file"], 'r')
 
             for line in file:
