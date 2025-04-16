e65886241943c2a3efbe7d83daed309bfbdce987
Jared <jared.ekenstam@gmail.com>
2025-04-16 10:27:20 -0700

    environment variables

--- games.py
+++ games.py
@@ -1,80 +1,102 @@
+import docker
 import os
-import keyboard
-import time
 
-def clear_terminal_inputs():
-    keyboard.press_and_release("enter")
-    time.sleep(1)
-
-#Keyboard module is used to emulate key presses to send commands to already-active terminal processes. Because emulated keys for capital letters and characters accessed by shift key don't physically exist on keyboard, this function can be used to enter special characters if necessary.
-#CURRENTLY DOES NOT WORK IN LINUX ENVIRONMENT - Observed behavior is that using the shift key through the keyboard module in Linux will result in locking keyboard inputs into the shift layer, which breaks all further inputs. Unsure if this is a fixable bug or a limitation of the module.
-def parse_text(text):
-    special_chars = {"~":"`","!":"1","@":"2","#":"3","$":"4","%":"5","^":"6","&":"7","*":"8","(":"9",")":"0","_":"-","+":"=","<":",",">":".","?":"/","{":"[",", }":"]","|":"\\"}
-    for char in text:
-        if char.isalpha() == True and char.isupper() == True:
-            if char.isupper() == True:
-                char = char.lower()
-                keyboard.press_and_release(f"shift+{char}")
-        elif char in special_chars.keys():
-            keyboard.press_and_release(f"shift+{special_chars[char]}")
-        else:
-            keyboard.write(char)
-#GameServer class is used to store information about game servers, such as name(string), icon(filename string sans extension), ports(list), path(string), log file(string), and commands to start and stop the server. Additional commands may be added to any individual game server to make use of functionality made available through that game's own server software.
-# 
-# The class also has a method to check if the server is running, and a method to execute a command on the server. 
 class GameServer:
-    def __init__(self, name, icon, port, path, log_file, cmds) -> None:
-        self.name = name #String, name of the game server
-        self.icon = icon #string, filename sans extension of the game server icon
-        self.ports = port #List, ports used by the game server. This is used to check if the port is listening to determine if the server is running.
-        self.path = path #string, path to the game server's main directory
-        self.log_file = log_file #dictionary, contains information about the game server's log file, including the file path, the string that indicates a player has connected, the string that indicates a player has disconnected, and the splice points to extract the player name from the log file. 
-        self.cmds = cmds #dictionary, contains the commands to start and stop the game server. Additional commands may be added to this dictionary to make use of additional functionality provided by the game server software. The start command may be either a single string or a list of strings, this is used to start a server in the instance where a single command is impractical.
+    def __init__(self, name, icon, ports, protocol, image, container_name, env_vars=None, volume=None) -> None:
+        self.name = name  # String, name of the game server
+        self.icon = icon  # String, filename sans extension of the game server icon
+        self.ports = ports  # List, ports used by the game server
+        self.protocol = protocol # String, protocol used by the game server (e.g., "tcp", "udp")
+        self.image = image  # String, Docker image name
+        self.container_name = container_name  # String, Docker container name
+        self.env_vars = env_vars or {}  # Dictionary, environment variables for the container
+        self.volume = volume  # String, Docker volume for persistent storage
         self.running = False
+        self.client = docker.from_env()
 
     def check_if_running(self):
-        self.running = False
-        os.system("ss -l > running_jobs.txt")
-        f = open("running_jobs.txt", "r")
-        file_contents = f.readlines()
-        for line in file_contents:
-            for port in self.ports:
-                if str(port) in line:
-                    self.running = True
-                else:
-                    pass
+        try:
+            container = self.client.containers.get(self.container_name)
+            self.running = container.status == "running"
+        except docker.errors.NotFound:
+            self.running = False
 
     def exec_cmd(self, command):
-        clear_terminal_inputs()
+        self.check_if_running()
         if command == "start":
-            self.check_if_running()
             if not self.running:
-                keyboard.write("cd " + self.path)
-                keyboard.press_and_release("enter")
-            else:
-                return (f"{self.name} already running")
-        hotkeys = ["ctrl", "shift", "alt"]
-        for key in hotkeys:
-            if key in self.cmds[command]:
-                hotkey = self.cmds[command].split(",")
-                keyboard.press_and_release(f"{hotkey[0]}+{hotkey[1]}")
-                return command
-        if type(self.cmds[command]) == list:
-            for cmd in self.cmds[command]:
-                parse_text(cmd)
-                keyboard.press_and_release("enter")
-        else:
-            parse_text(self.cmds[command])
-        keyboard.press_and_release("enter")
-        return command
 
-#Define the game list and add individual game server objects to it. Refer to the GameServer class for more information on the parameters required to create a new game server object.
+                self.client.containers.run(
+                    self.image,
+                    name=self.container_name,
+                    ports={f"{port}/{self.protocol}": port for port in self.ports},
+                    environment=self.env_vars,
+                    volumes={
+                        self.volume: {
+                            'bind': '/data',  # Target path in container
+                            'mode': 'rw'  # Read-write mode
+                        }
+                    },
+                    detach=True,
+                )
+                return f"{self.name} started"
+            else:
+                return f"{self.name} is already running"
+        elif command == "stop":
+            if self.running:
+                container = self.client.containers.get(self.container_name)
+                container.stop()
+                container.remove()
+                return f"{self.name} stopped"
+            else:
+                return f"{self.name} is not running"
+        else:
+            return f"Unknown command: {command}"
+
+# Define the game list and add individual game server objects to it
 game_list = []
 
-minecraft_serv = GameServer("Minecraft", "minecraft", [25565], "/home/gameserver/minecraft/", {"file": "/home/gameserver/minecraft/logs/latest.log",  "connect": "joined the game", "disconnect": "left the game", "splice_join_start": 33, "splice_left_start": 33, "splice_join_end": -13, "splice_left_end": -13}, {"start": "java -jar server.jar nogui", "stop": "/stop"})
-val_serv = GameServer("Valheim", "valheim", [2456, 2457], "/home/gameserver/valheim/", {"file": "/home/gameserver/valheim/valheim_log.txt", "connect": "Got handshake from client", "disconnect": "Closing socket", "splice_join_start": 47, "splice_left_start": 36, "splice_join_end": 64, "splice_left_end": 53}, {"start": ". start-server.sh", "stop": "ctrl,c"})
-seven_days_serv = GameServer("7 Days to Die", "7days", [26900,26901,26902], "/home/gameserver/7-days-to-die/", {"file": "/home/gameserver/7-days-to-die/log.txt", "connect": "' joined the game", "disconnect":"' left the game", "splice_join_start": 49, "splice_left_start": 49, "splice_join_end": -14, "splice_left_end": -14}, {"start": ". startserver.sh -configfile='serverconfig.xml'", "stop" : "ctrl,c"})
-pal_server = GameServer("Palworld", "palworld", [8211], "/home/gameserver/palworld/", {"file" : "/home/gameserver/palworld/log.txt", "connect": "joined the server.", "disconnect": "left the server.", "splice_join_start": 28, "splice_left_start": 28, "splice_join_end": -54, "splice_left_end": -52}, {"start": ["cp /home/gameserver/palworld/DefaultPalWorldSettings.old.ini /home/gameserver/palworld/Pal/Saved/Config/LinuxServer/PalWorldSettings.ini ",". palserver.sh"], "stop": "ctrl,c"})
+minecraft_serv = GameServer(
+    "Minecraft",
+    "minecraft",
+    [25565],
+    "tcp",
+    "itzg/minecraft-server",
+    "minecraft_server",
+    {"EULA": "TRUE"},
+    "/home/gameserver/minecraft/"
+)
+val_serv = GameServer(
+    "Valheim",
+    "valheim",
+    [2456, 2457],
+    "udp",
+    "lloesche/valheim-server",
+    "valheim_server",
+    {"SERVER_NAME": "ValheimServer", "WORLD_NAME": "MyWorld", "SERVER_PASS": "secret"},
+    "/home/gameserver/valheim/"
+)
+seven_days_serv = GameServer(
+    "7 Days to Die",
+    "7days",
+    [26900, 26901, 26902],
+    "tcp",
+    "didstopia/7dtd-server",
+    "7days_server",
+    {},
+    "/home/gameserver/minecraft/"
+)
+pal_server = GameServer(
+    "Palworld",
+    "palworld",
+    [8211],
+    "tcp",
+    "palworld/server-image",
+    "palworld_server",
+    {},
+    "/home/gameserver/palworld/"
+)
+
 game_list.append(minecraft_serv)
 game_list.append(val_serv)
 game_list.append(seven_days_serv)
--- server.py
+++ server.py
@@ -1,108 +1,35 @@
 from flask import Flask, send_file
 from flask_cors import CORS
-from threading import Timer
 from games import *
 import datetime
 import json
+import dotenv
 
 app = Flask(__name__)
 CORS(app)
+dotenv.load_dotenv()
 active_server = ""
-delay = False
-delay_time = datetime.datetime.now()
-connected_players = [] 
-lease_time = 0 #value in minutes to block additional start/stop commands after a new game server instance is begun. 0 means server will allow all start/stop commands always.
-empty_time = datetime.datetime.now()
-max_empty_time = 20 #value in minutes to allow server to be empty before stopping it. 0 means server will never stop due to inactivity.
+connected_players = []
 
-#function used to check if server lease time has elapsed
-def checktime():
-    global delay
-    check_time = datetime.datetime.now()
-    difference = (check_time.minute + (check_time.hour * 60)) - (delay_time.minute + (delay_time.hour * 60))
-    if difference >= lease_time:
-        delay = False
-    else:
-        return
 
-#timer object to initiate periodic status update checks
-class RepeatedTimer(object):
-    def __init__(self, interval, function):
-        self._timer = None
-        self.interval = interval
-        self.function = function
-        self.is_running = False
-        self.start()
-    def _run(self):
-        self.is_running = False
-        self.start()
-        self.function()
-
-    def start(self):
-        if not self.is_running:
-            self._timer = Timer(self.interval, self._run)
-            self._timer.start()
-            self.is_running = True
-
-    def stop(self):
-        self._timer.cancel()
-        self.is_running = False
-
-#Function to periodically check if a server is running, and if so, to pull from game's most recent log data to determine connected players
-#this function also checks if the server has been empty for a set amount of time, and if so, sets a timer to stop the server
-def update_status():
-    checktime()
-    global delay
-    global active_server
-    global connected_players
-    global empty_time
-    global max_empty_time
-    active_server = ""
-
-    for game in game_list:
-        game.check_if_running()
-        if game.running == True:
-            active_server = game.name
-            if game.log_file["file"] is not None:
-                file = open(game.log_file["file"], 'r')
-                for line in file:
-                    if game.log_file["connect"] in line:
-                        parsed_name = line[game.log_file["splice_join_start"]:game.log_file["splice_join_end"]]
-                        if not parsed_name in connected_players:
-                            connected_players.append(parsed_name)
-                    elif game.log_file["disconnect"] in line:
-                        parsed_name = line[game.log_file["splice_left_start"]:game.log_file["splice_left_end"]]
-                        if parsed_name in connected_players:
-                            connected_players.remove(parsed_name)
-                if len(connected_players) > 0 and max_empty_time > 0:
-                    empty_time = datetime.datetime.now()
-                file.close()
-    if active_server == "":
-        for player in connected_players:
-            connected_players.remove(player)
-        delay = False
-        empty_time = datetime.datetime.now()
-        
-    if max_empty_time > 0 and len(connected_players) == 0 and active_server != "":
-        empty_check = datetime.datetime.now()
-        difference = (empty_check.minute + (empty_check.hour * 60)) - (empty_time.minute + (empty_time.hour * 60))
-        if difference >= max_empty_time:
-            for game in game_list:
-                if game.running == True:
-                    game.exec_cmd("stop")
-            empty_time = datetime.datetime.now()
-
-rt = RepeatedTimer(2, update_status)
-
-@app.route('/')
-def home():
-    return "No. Go away."
 @app.route('/update')
 def serv_stats():
     server_list = []
     for game in game_list:
-        server_list.append({"name" : game.name, "icon": game.icon, "status": game.running, "port": game.ports[0]})
-    return json.dumps({"active_server" : active_server, "player_count" : len(connected_players),"players" : connected_players, "returnval" : "0", "games": server_list})
+        game.check_if_running()
+        server_list.append({
+            "name": game.name,
+            "icon": game.icon,
+            "status": game.running,
+            "port": game.ports[0]
+        })
+    return json.dumps({
+        "active_server": active_server,
+        "player_count": len(connected_players),
+        "players": connected_players,
+        "games": server_list
+    })
+
 @app.route('/image/<gameid>')
 def return_image(gameid):
     for game in game_list:
@@ -110,45 +37,29 @@ def return_image(gameid):
             image_path = f"static/{game.icon}.png"
             return send_file(image_path, mimetype='image/png')
     return "No image found"
-@app.route('/<gameid>')
-def gamecheck(gameid):
-    for game in game_list:
-        if game.name.lower() == gameid.lower():
-            return str(game.running)
 
+
+#Server commands presently only support start and stop. More commands will be added specific to each game at a later date.
 @app.route('/<gameid>/<cmd>')
 def exec_cmd_on_game(gameid, cmd):
-    gameid = gameid
-    cmd = cmd
-    global delay_time
     global active_server
-    global delay
-    global empty_time
     for game in game_list:
         if game.name.lower() == gameid.lower():
-            if cmd == "start":
-                if not delay and active_server == "":
-                    delay = True
-                    delay_time = datetime.datetime.now()
-                    active_server = game.name
-                    empty_time = datetime.datetime.now()
-                else:
-                    return("Server is leased. Please try again later.")
-            elif cmd == "stop":
-                if active_server == game.name and delay == False:
-                    active_server = ""
-                    
-                else:
-                    return json.dumps("Server not running")
-            returnval = game.exec_cmd(cmd)
-            return json.dumps({"active_server" : active_server, "player_count": len(connected_players), "players" : connected_players, "returnval": returnval})
-        
+            result = game.exec_cmd(cmd)
+            if cmd == "start" and "started" in result:
+                active_server = game.name
+            elif cmd == "stop" and "stopped" in result:
+                active_server = ""
+            return json.dumps({
+                "active_server": active_server,
+                "player_count": len(connected_players),
+                "players": connected_players,
+                "result": result
+            })
+    return json.dumps({"error": "Game not found"})
 
 if __name__ == "__main__":
-    try:
-      print("Attempting to start HTTPS server.")
-      app.run(ssl_context=('/etc/letsencrypt/archive/games.caramellatte.dev/fullchain6.pem', '/etc/letsencrypt/archive/games.caramellatte.dev/privkey6.pem'), host="0.0.0.0", port=8080)
-    except:
-        print("Server cannot start, aborting")
-
-rt.stop()
+    chain = os.getenv("SSL_CHAIN")
+    privkey = os.getenv("SSL_PRIVKEY")
+    print(chain, privkey)
+    app.run(ssl_context=('/etc/letsencrypt/archive/games.caramellatte.dev/fullchain6.pem', '/etc/letsencrypt/archive/games.caramellatte.dev/privkey6.pem'), host="0.0.0.0", port=8080)
