78537a479b8a5cdc86f191bc175b46ab7cb79744
Jared <jared.ekenstam@gmail.com>
2025-09-26 08:01:56 -0700

    more tests

--- games.py
+++ games.py
@@ -200,7 +200,7 @@ val_serv = GameServer(
     "valheim",
     [2456, 2457],
     "valheim-modded:latest",
-    "valheim-modded",
+    "valheim-modded:latest",
     {"SERVER_NAME": "ValheimServer", "WORLD_NAME": "nerds", "SERVER_PASS": "secret"},
     "/home/gameserver/valheim/",
     {
